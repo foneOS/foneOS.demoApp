af0781a9a7bb969f9983bf609a52715e6a56b05a
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-12-11 08:56:42 -0500

    Moving app

---
+++ lua/main.lua
@@ -0,0 +1,23 @@
+scr = FoneOSScreen.new()
+
+title = FoneOSTitle.new()
+title:text("foneOS demo app")
+scr:addTitle(title)
+
+label = FoneOSLabel.new()
+label:x(5)
+label:y(30)
+label:text("This message is from\na Lua script!\n\n" .. math.random())
+scr:addLabel(label)
+
+btn = FoneOSButton.new()
+btn:x(5)
+btn:y(200)
+btn:text("Press me!")
+btn:create()
+btn:onActivate(function()
+	fone.layout.test("Pressed!")
+end)
+scr:addButton(btn)
+
+fone.layout.current(scr)
